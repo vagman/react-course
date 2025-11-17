bb420d163a72299e8a20db74ad9b45ce5e310859
vagman <bmanousakis@gmail.com>
2025-11-17 21:11:46 +0200

    rendering lists with JSX

--- 03-pizza-menu/src/App.jsx
+++ 03-pizza-menu/src/App.jsx
@@ -68,10 +68,26 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      <Pizza />
-      <Pizza />
-      <Pizza />
+
+      <ul className="pizzas">
+        {pizzaData.map(pizza => (
+          <Pizza pizzaObj={pizza} key={pizza.name} />
+        ))}
+      </ul>
     </main>
+  );
+}
+
+function Pizza(props) {
+  return (
+    <li className="pizza">
+      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
+      <div>
+        <h3>{props.pizzaObj.name}</h3>
+        <p>{props.pizzaObj.ingredients}</p>
+        <span>{props.pizzaObj.price} €</span>
+      </div>
+    </li>
   );
 }
 
@@ -89,16 +105,4 @@ function Footer() {
 
   return <footer className="footer">{new Date().toLocaleTimeString()}. We are currently open!</footer>;
   // return React.createElement('footer', null, 'We are currently open!');
-}
-
-const Test = () => {};
-
-function Pizza() {
-  return (
-    <div>
-      <img src="pizzas/spinaci.jpg" alt="Pizza Spinaci" />
-      <h3>Pizza Spinaci</h3>
-      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
-    </div>
-  );
 }
