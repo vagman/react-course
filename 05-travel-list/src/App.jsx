bfa2dfc01480e49392ab40faf97e79bc2966ba48
vagman <bmanousakis@gmail.com>
2025-11-21 16:55:43 +0200

    controlled elements

--- 05-travel-list/src/App.jsx
+++ 05-travel-list/src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 const initialItems = [
   { id: 1, description: 'Passports', quantity: 2, packed: false },
   { id: 2, description: 'Socks', quantity: 12, packed: true },
@@ -20,22 +22,37 @@ function Logo() {
 }
 
 function Form() {
+  const [description, setDescription] = useState('');
+  const [quantity, setQuantity] = useState(1);
+
   function handleSubmit(e) {
     e.preventDefault();
-    console.log('Submitted');
+
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+
+    setDescription('');
+    setQuantity(1);
   }
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 🥰 trip ?</h3>
-      <select>
+      <select value={quantity} onChange={event => setQuantity(Number(event.target.value))}>
         {Array.from({ length: 20 }, (_, i) => i + 1).map(element => (
           <option value={element} key={element}>
             {element}
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Item..." />
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={event => setDescription(event.target.value)}
+      />
       <button>Add</button>
     </form>
   );
