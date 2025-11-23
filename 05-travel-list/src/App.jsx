82b18004afc863df9033c7d9bdb5174baa86480b
vagman <bmanousakis@gmail.com>
2025-11-23 19:00:16 +0200

    deleting an item: parent to child communication

--- 05-travel-list/src/App.jsx
+++ 05-travel-list/src/App.jsx
@@ -1,17 +1,22 @@
 import { useState } from 'react';
 
-const initialItems = [
-  { id: 1, description: 'Passports', quantity: 2, packed: false },
-  { id: 2, description: 'Socks', quantity: 12, packed: true },
-  { id: 3, description: 'Charger', quantity: 1, packed: false },
-];
-
 function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems(items => [...items, item]);
+  }
+
+  function handleDeleteItem(id) {
+    console.log('Delete item with id:', id);
+    setItems(items => items.filter(item => item.id !== id));
+  }
+
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={handleAddItems} />
+      <PackingList items={items} onDeleteItem={handleDeleteItem} />
       <Statistics />
     </div>
   );
@@ -21,7 +26,7 @@ function Logo() {
   return <h3>🌴 Far Away 💼</h3>;
 }
 
-function Form() {
+function Form({ onAddItems }) {
   const [description, setDescription] = useState('');
   const [quantity, setQuantity] = useState(1);
 
@@ -32,6 +37,8 @@ function Form() {
 
     const newItem = { description, quantity, packed: false, id: Date.now() };
     console.log(newItem);
+
+    onAddItems(newItem);
 
     setDescription('');
     setQuantity(1);
@@ -58,12 +65,12 @@ function Form() {
   );
 }
 
-function PackingList() {
+function PackingList({ items, onDeleteItem }) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map(item => (
-          <Item item={item} key={item.id}>
+        {items.map(item => (
+          <Item item={item} key={item.id} onDeleteItem={() => onDeleteItem(item.id)}>
             {' '}
           </Item>
         ))}
@@ -72,13 +79,13 @@ function PackingList() {
   );
 }
 
-function Item({ item }) {
+function Item({ item, onDeleteItem }) {
   return (
     <li>
       <span style={item.packed ? { textDecoration: 'line-through' } : {}}>
         {item.quantity} {item.description}
       </span>
-      <button>❌</button>
+      <button onClick={() => onDeleteItem(item.id)}>❌</button>
     </li>
   );
 }
