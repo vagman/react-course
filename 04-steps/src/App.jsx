92837a92df6153b9bbb59d8931ccf82e84447c3d
vagman <bmanousakis@gmail.com>
2025-11-27 12:18:15 +0200

    even more reusability with children prop

--- 04-steps/src/App.jsx
+++ 04-steps/src/App.jsx
@@ -3,11 +3,27 @@ import { useState } from 'react';
 const messages = ['Learn React ⚛️', 'Apply for jobs 💼', 'Invest your new income 🤑'];
 
 function App() {
+  return (
+    <div>
+      <Steps />
+      {/* <Steps />
+      <Steps /> */}
+      <StepMessage step={1}>
+        <p>Pass in content</p>
+        <p>👋</p>
+      </StepMessage>
+      <StepMessage step={2}>
+        <p>Read children prop</p>
+        <p>🫡</p>
+      </StepMessage>
+    </div>
+  );
+}
+
+function Steps() {
   // React hook to manage the current step state
   const [step, setStep] = useState(1);
   const [isOpen, setIsOpen] = useState(true);
-
-  //   const [test, setTest] = useState({ name: 'Jonas' });
 
   function handlePrevious() {
     if (step > 1) setStep(step - 1);
@@ -15,12 +31,6 @@ function App() {
 
   function handleNext() {
     if (step < 3) setStep(step + 1);
-
-    // Bad Practise:
-    // test.name= 'Fred';
-
-    // Good Practise:
-    // setTest({ name: 'Fred' });
   }
 
   return (
@@ -35,9 +45,16 @@ function App() {
             <div className={step >= 2 ? 'active' : ''}>2</div>
             <div className={step >= 3 ? 'active' : ''}>3</div>
           </div>
-          <p className="message">
-            Step {step}: {messages[step - 1]}
-          </p>
+
+          <StepMessage step={step}>
+            {messages[step - 1]}
+            <div className="buttons">
+              <Button bgColor="#e7e7e7" textColor="#333" onClick={() => alert(`Learn how to ${messages[step - 1]}`)}>
+                Learn how
+              </Button>
+            </div>
+          </StepMessage>
+
           <div className="buttons">
             <Button bgColor="#7950f2" txtColor="#fff" onClick={handlePrevious}>
               <span>⬅️</span>Previous
@@ -49,6 +66,15 @@ function App() {
         </div>
       )}
     </>
+  );
+}
+
+function StepMessage({ step, children }) {
+  return (
+    <div className="message">
+      <h3>Step {step}:</h3>
+      {children}
+    </div>
   );
 }
 
