da827145d47e519faea384eea8076e8606a6c341
vagman <bmanousakis@gmail.com>
2025-11-30 14:50:20 +0200

    implemented the static part of the app

--- 06-eat-n-split/src/App.jsx
+++ 06-eat-n-split/src/App.jsx
@@ -21,9 +21,46 @@ const initialFriends = [
 
 function App() {
   return (
-    <div>
-      <h1>Eat n Split</h1>
+    <div className="app">
+      <div className="sidebar">
+        <FriendsList />
+      </div>
     </div>
+  );
+}
+
+function FriendsList() {
+  const friends = initialFriends;
+  return (
+    <ul>
+      {friends.map(friend => (
+        <Friend friend={friend} key={friend.id} />
+      ))}
+    </ul>
+  );
+}
+
+function Friend({ friend }) {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+      {friend.balance < 0 && (
+        <p className="red">
+          You owe {friend.name} {Math.abs(friend.balance)}€.
+        </p>
+      )}
+
+      {friend.balance > 0 && (
+        <p className="green">
+          Your friend {friend.name} owes you {Math.abs(friend.balance)}€.
+        </p>
+      )}
+
+      {friend.balance === 0 && <p>You and {friend.name} are even.</p>}
+
+      <button className="button">Select</button>
+    </li>
   );
 }
 
