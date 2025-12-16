627bdb17cfb80bac074b6ed4a8b32f78675755f7
vagman <bmanousakis@gmail.com>
2025-12-16 11:05:34 +0200

    initialize Section 11 - 08-how-react-works with Vite instead of create-react-app

--- 08-how-react-works/src/App.jsx
+++ 08-how-react-works/src/App.jsx
@@ -1,24 +1,24 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 const content = [
   {
-    summary: "React is a library for building UIs",
+    summary: 'React is a library for building UIs',
     details:
-      "Dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      'Dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
   },
   {
-    summary: "State management is like giving state a home",
+    summary: 'State management is like giving state a home',
     details:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
   },
   {
-    summary: "We can think of props as the component API",
+    summary: 'We can think of props as the component API',
     details:
-      "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+      'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
   },
 ];
 
-export default function App() {
+function App() {
   return (
     <div>
       <Tabbed content={content} />
@@ -38,21 +38,14 @@ function Tabbed({ content }) {
         <Tab num={3} activeTab={activeTab} onClick={setActiveTab} />
       </div>
 
-      {activeTab <= 2 ? (
-        <TabContent item={content.at(activeTab)} />
-      ) : (
-        <DifferentContent />
-      )}
+      {activeTab <= 2 ? <TabContent item={content.at(activeTab)} /> : <DifferentContent />}
     </div>
   );
 }
 
 function Tab({ num, activeTab, onClick }) {
   return (
-    <button
-      className={activeTab === num ? "tab active" : "tab"}
-      onClick={() => onClick(num)}
-    >
+    <button className={activeTab === num ? 'tab active' : 'tab'} onClick={() => onClick(num)}>
       Tab {num + 1}
     </button>
   );
@@ -72,9 +65,7 @@ function TabContent({ item }) {
       {showDetails && <p>{item.details}</p>}
 
       <div className="tab-actions">
-        <button onClick={() => setShowDetails((h) => !h)}>
-          {showDetails ? "Hide" : "Show"} details
-        </button>
+        <button onClick={() => setShowDetails(h => !h)}>{showDetails ? 'Hide' : 'Show'} details</button>
 
         <div className="hearts-counter">
           <span>{likes} ❤️</span>
@@ -98,3 +89,5 @@ function DifferentContent() {
     </div>
   );
 }
+
+export default App;
