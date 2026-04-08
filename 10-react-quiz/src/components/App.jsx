1c3ea9a84d3b7c958b5530be400aa9084da9d797
vagman <bmanousakis@gmail.com>
2026-04-08 22:06:24 +0300

    Addition of progress bar

--- 10-react-quiz/src/components/App.jsx
+++ 10-react-quiz/src/components/App.jsx
@@ -2,10 +2,12 @@ import { useEffect, useReducer } from 'react';
 
 import StartScreen from './StartScreen.jsx';
 import Question from './Question.jsx';
+import NextButton from './NextButton.jsx';
 import Header from './Header.jsx';
 import Main from './Main.jsx';
 import Loader from './Loader.jsx';
 import Error from './Error.jsx';
+import Progress from './Progress.jsx';
 
 const initialState = {
   questions: [],
@@ -17,7 +19,7 @@ const initialState = {
 };
 
 function reducer(state, action) {
-  const question = state.questions.at(state.index);
+  const question = state.questions.at(state.questionIndex);
 
   switch (action.type) {
     case 'dataReceived':
@@ -39,15 +41,18 @@ function reducer(state, action) {
         answer: action.payload,
         points: action.payload === question.correctOption ? state.points + question.points : state.points,
       };
+    case 'nextQuestion':
+      return { ...state, questionIndex: state.questionIndex + 1, answer: null };
     default:
       throw new Error('Action unknown');
   }
 }
 
 function App() {
-  const [{ questions, status, questionIndex, answer }, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status, questionIndex, answer, points }, dispatch] = useReducer(reducer, initialState);
 
   const numQuestions = questions.length;
+  const maxPossiblePoints = questions.reduce((previous, current) => previous + current.points, 0);
 
   useEffect(function () {
     fetch('http://localhost:8000/questions')
@@ -63,7 +68,19 @@ function App() {
         {status === 'loading' && <Loader />}
         {status === 'error' && <Error />}
         {status === 'ready' && <StartScreen numQuestions={numQuestions} dispatch={dispatch} />}
-        {status === 'active' && <Question question={questions[questionIndex]} dispatch={dispatch} answer={answer} />}
+        {status === 'active' && (
+          <>
+            <Progress
+              index={questionIndex}
+              numberOfQuestions={numQuestions}
+              points={points}
+              maxPossiblePoints={maxPossiblePoints}
+              answer={answer}
+            />
+            <Question question={questions[questionIndex]} dispatch={dispatch} answer={answer} />
+            <NextButton dispatch={dispatch} answer={answer} />
+          </>
+        )}
       </Main>
     </div>
   );
---
+++ 10-react-quiz/src/components/Progress.jsx
@@ -0,0 +1,17 @@
+function Progress({ index, numberOfQuestions, points, maxPossiblePoints, answer }) {
+  return (
+    <header className="progress">
+      <progress max={numberOfQuestions} value={index + Number(answer !== null)} />
+      <p>
+        Question <strong>{index + 1}</strong> / {numberOfQuestions}
+      </p>
+      <p>
+        <strong>
+          {points} / {maxPossiblePoints}
+        </strong>
+      </p>
+    </header>
+  );
+}
+
+export default Progress;
