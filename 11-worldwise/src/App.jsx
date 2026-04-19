be8df1ebba0ab3a9fe5d9ae9e9308941fb4d6506
vagman <bmanousakis@gmail.com>
2026-04-19 03:10:45 +0300

    Lecture 209. CSS Modules

--- 11-worldwise/src/App.jsx
+++ 11-worldwise/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Product from './pages/Product.jsx';
 import Homepage from './pages/Homepage.jsx';
 import Pricing from './pages/Pricing.jsx';
+import AppLayout from './pages/AppLayout.jsx';
 import PageNotFound from './pages/PageNotFound.jsx';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
+        <Route path="app" element={<AppLayout />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
