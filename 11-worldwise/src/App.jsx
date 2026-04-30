4aeb4e4303ab83a04a71abbdddef72ddd36c57a6
vagman <bmanousakis@gmail.com>
2026-04-30 20:19:16 +0300

    Lecture 213. Implementing Cities List

--- 11-worldwise/src/App.jsx
+++ 11-worldwise/src/App.jsx
@@ -1,4 +1,6 @@
+import { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { polyfillCountryFlagEmojis } from 'country-flag-emoji-polyfill';
 
 import Product from './pages/Product.jsx';
 import Homepage from './pages/Homepage.jsx';
@@ -6,8 +8,32 @@ import Pricing from './pages/Pricing.jsx';
 import Login from './pages/Login.jsx';
 import AppLayout from './pages/AppLayout.jsx';
 import PageNotFound from './pages/PageNotFound.jsx';
+import CityList from './components/CityList.jsx';
+
+polyfillCountryFlagEmojis();
+
+const URL = 'http://localhost:9000';
 
 function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const result = await fetch(`${URL}/cities`);
+        const data = await result.json();
+        setCities(data);
+      } catch (error) {
+        alert(`There was an error ${error}`);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
@@ -16,8 +42,8 @@ function App() {
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
-          <Route index element={<p>List of cities</p>} />
-          <Route path="cities" element={<p>List of cities</p>} />
+          <Route index element={<CityList cities={cities} isLoading={isLoading} />} />
+          <Route path="cities" element={<CityList cities={cities} isLoading={isLoading} />} />
           <Route path="countries" element={<p>List of countries</p>} />
           <Route path="form" element={<p>Form</p>} />
         </Route>
